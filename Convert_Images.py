2e0693f84e7487b40eeac394db5496cb667a9db0
pauloladapo13 <72332084+pauloladapo13@users.noreply.github.com>
2021-09-03 22:00:51 +0200

    Image tools1

--- Convert_Images.py
+++ Convert_Images.py
@@ -1,5 +1,14 @@
 from PIL import Image
+import os
 
-with Image.open('empire1.jpg') as im:
+filelist = ''
 
-    im.convert('L').show()
+for infile in filelist:
+    outfile = os.path.splitext(infile)[0] + 'JPEG'
+    if infile != outfile:
+        try:
+            with Image.open(infile).save(outfile) as newim:
+                newim.rotate(0).show()
+        except IOError:
+
+            print('cannot convert', infile)
